54ecd3dd321fd957f91e3eb429f185dd9c3f619c
Josh The Human <47869156+JoshTheHumanBean@users.noreply.github.com>
2021-09-20 14:58:03 -0500

    Adding the initial files

---
+++ src/Banking.java
@@ -0,0 +1,34 @@
+public class Banking {
+    private String name;
+    private double balance;
+
+    public Banking(String name, double balance){
+        this.name = name;
+
+        if (balance > 0.0) this.balance = balance;
+    }
+
+    public void deposit(double depositAmount){
+        if (depositAmount > 0.0) balance += depositAmount;
+    }
+
+    public void withdraw(double withdrawlAmount){
+        if (withdrawlAmount > 0.0) balance -= withdrawlAmount;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
