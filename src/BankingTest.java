6819050ea9d5f3fe6a86b20714ba10fbfd84fbad
Josh The Human <47869156+JoshTheHumanBean@users.noreply.github.com>
2021-10-25 15:08:03 -0500

    New change

--- src/BankingTest.java
+++ src/BankingTest.java
@@ -33,7 +33,7 @@ public class BankingTest {
         Banking account3 = new Banking();
 
         System.out.printf("%n%nNew Account Name: ");
-<<<<<<< HEAD
+
         account3.setName(scanner.next());
 
         System.out.printf("%nNew Account's Initial Balance: ");
@@ -43,20 +43,17 @@ public class BankingTest {
         Banking.printInfo(account2);
         Banking.printInfo(account3);
 
-=======
+
         String temp1 = scanner.next();
-        scanner.nextLine();
-        account3.setName(temp1);
+        account3.setName(scanner.nextLine());
         System.out.printf("%nNew Account's Initial Balance: ");
-        double temp2 = scanner.nextDouble();
-        scanner.nextLine();
-        account3.setBalance(temp2);
+        account3.setBalance(scanner.nextDouble());
 
         Banking.printInfo(account1);
         Banking.printInfo(account2);
         Banking.printInfo(account3);
 
->>>>>>> aba6ade8e6b2a82f9071fcf3235b5bf78eabe091
+
     
     }
 }
