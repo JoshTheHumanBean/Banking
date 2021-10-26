89192be4c01f00773ff864bf50f1b9347d8ed6b4
Josh The Human <47869156+JoshTheHumanBean@users.noreply.github.com>
2021-10-26 14:55:10 -0500

    Created methods to create and initialize arrays of Banking objects

--- src/BankingArray.java
+++ src/BankingArray.java
@@ -1,2 +1,16 @@
 public class BankingArray {
+    private static int numAccounts = 0;
+
+    public static void createAccounts(int i){
+        Banking[] accounts = new Banking[numAccounts+i];
+        numAccounts += i;
+    }
+
+    public static void initializeAccounts(Banking[] objects){
+        for (int i =0; i<numAccounts; i++){
+            if (objects[i] == null){
+                Banking.initializeAccount(objects[i]);
+            }
+        }
+    }
 }
