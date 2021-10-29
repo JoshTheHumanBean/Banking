34af3a1861fa7391447f1135101b979e346c2490
Josh The Human <47869156+JoshTheHumanBean@users.noreply.github.com>
2021-10-29 13:46:33 -0500

    Added FileNotFoundException handeler in BankingArray.deserializeAccounts.

--- src/BankingArray.java
+++ src/BankingArray.java
@@ -8,6 +8,12 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Objects;
+import org.w3c.dom.*;
+import org.xml.sax.*;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 public class BankingArray extends Banking{
     private static int numAccounts = 0;
@@ -75,11 +81,15 @@ public class BankingArray extends Banking{
     }
 
     public static void deserializeAccounts() throws IOException{
-        FileInputStream fis = new FileInputStream("accounts.xml");
-        XMLDecoder decoder = new XMLDecoder(fis);
-        accountsList.addAll((ArrayList<Banking>) decoder.readObject());
-        decoder.close();
-        fis.close();
+        try{
+            FileInputStream fis = new FileInputStream("accounts.xml");
+            XMLDecoder decoder = new XMLDecoder(fis);
+            accountsList.addAll((ArrayList<Banking>) decoder.readObject());
+            decoder.close();
+            fis.close();
+        }catch(java.io.FileNotFoundException file){
+            System.out.printf("File not found; Action canceled%n");
+        }
     }
 
     public static void changePin(Banking object){
@@ -89,9 +99,40 @@ public class BankingArray extends Banking{
             Banking.setPin(input.next(), object);
         }
     }
-
-
-
+    /*
+    public static void deleteAccount(){
+        try{
+            BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+            String xmlFile = accounts.xml;
+            File file = new File(xmlFile);
+            System.out.print("Enter an element which have to delete: ");
+            String remElement = bf.readLine();
+            if (file.exists()){
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                Document doc = builder.parse(xmlFile);
+                TransformerFactory tFactory = TransformerFactory.newInstance();
+                Transformer tFormer = tFactory.newTransformer();
+                Element element = (Element)doc.getElementsByTagName(remElement).item(0);
+                //  Remove the node
+                element.getParentNode().removeChild(element);
+                //  Normalize the DOM tree to combine all adjacent nodes
+                doc.normalize();
+                Source source = new DOMSource(doc);
+                Result dest = new StreamResult(System.out);
+                tFormer.transform(source, dest);
+                System.out.println();
+            }
+            else{
+                System.out.println("File not found!");
+            }
+        }
+        catch (Exception e){
+            System.err.println(e);
+            System.exit(0);
+        }
+    }
+    */
     public static void mainMenu(){
         int choice;
             do {
