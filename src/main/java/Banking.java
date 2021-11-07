c574e5ef260e94bee9c1792b097dc01c8907d8c8
Josh The Human <47869156+JoshTheHumanBean@users.noreply.github.com>
2021-11-07 10:31:46 -0600

    Reformatting the null constructor didn't work, reverting to the older form

--- src/main/java/Banking.java
+++ src/main/java/Banking.java
@@ -11,7 +11,12 @@ public class Banking implements java.io.Serializable{
     public static Scanner input = new Scanner(System.in);
     public static Gson gson = new Gson();
 
-    public Banking(){this("", "", 0, "0000");}
+    public Banking(){
+        this.firstName = "";
+        this.lastName = "";
+        this.balance = 0.0;
+        this.pin = "0000";
+    }
 
     public Banking(String firstName, String lastName, double balance, String number){
         this.firstName = firstName;
